e69ba8b408e4a7161dff59fd81e90cf0b0ee1e43
Weilun Chen <weilunchen@Weiluns-MacBook-Pro.local>
2020-05-13 00:20:56 -0700

    Define Merkle tree API(1/n): This change defines Merkel tree struct and related messages.

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -1,7 +1,40 @@
-package main
+package merkel
 
-import "fmt"
+import (
+	_ "bytes"
+	_ "crypto/sha256"
+	_ "errors"
+	_ "hash"
+)
 
-func main() {
-	fmt.Println("vim-go")
+// This defines the object that will get stored in Merkle tree.
+type Content interface {
+	// This function returns SHA256 hash of the object.
+	CalcHash() ([]byte, error)
+	// Whether ths content equal another content.
+	Equals(content Content) (bool, error)
+}
+
+// The Merkle tree object. It contains pointer to contents that are stored in it, as well as the tree root.
+type MerkleTree struct {
+	Root         *Node
+	Leafs        []*Node
+}
+
+// This represents the Node that are stored in Merkle tree. A node becomes a leaf when Value is not nil.
+type Node struct {
+	Hash []byte
+	Left *Node
+	Right *Node
+	Value Content
+}
+
+// This function inits a Merkle tree, it takes in a list of Content, and init the entire tree.
+func (tree *MerkleTree) Init(contents []Content) error {
+	return nil
+}
+
+// This function returns a proof for
+func GetProof(tree *MerkleTree, content Content) (MerkleProof, error) {
+	return nil, nil
 }
