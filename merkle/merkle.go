d80c9518cb73ac4dfc6bcd33bb501bbb4f5d1a97
Weilun Chen <chenweilunster@gmail.com>
2020-05-14 22:59:14 -0700

    merkle tree (4/4): Finish all merkle tree related functionalities.

--- merkle/merkle.go
+++ merkle/merkle.go
@@ -10,24 +10,50 @@ import (
 
 // Content defines the object that will get stored in Merkle tree.
 type Content interface {
-	// This function returns SHA256 hash of the object.
+	// CalcHash returns SHA256 hash of the object.
 	CalcHash() ([]byte, error)
-	// Whether ths content equal another content.
+	// Equals compare with another another content.
 	Equals(content Content) (bool, error)
+	// DebugString output this content
+	DebugString() string
 }
 
 // MerkleTree contains pointer to contents that are stored in it, as well as the tree root.
 type MerkleTree struct {
-	Root         *Node
-	Leaves        []*Node
+	Root   *Node
+	Leaves []*Node
 }
 
 // Node represents the Node that are stored in Merkle tree. A node becomes a leaf when Value is not nil.
 type Node struct {
-	Hash []byte
-	Left *Node
-	Right *Node
-	Value *Content
+	Hash   []byte
+	Left   *Node
+	Right  *Node
+	Parent *Node
+	Value  *Content
+}
+
+func isSameBytes(left []byte, right []byte) bool {
+	if len(left) != len(right) {
+		return false
+	}
+	for i, val := range left {
+		if right[i] != val {
+			return false
+		}
+	}
+	return true
+}
+
+func getNodeSibling(node *Node) (*Node, error) {
+	if node.Parent == nil {
+		return nil, errors.New("parent doesn't have sibling")
+	}
+	parent := node.Parent
+	if isSameBytes(parent.Left.Hash, node.Hash) {
+		return parent.Right, nil
+	}
+	return parent.Right, nil
 }
 
 // Init inits a Merkle tree from contents, it takes in a list of Content, and init the entire tree.
@@ -35,17 +61,16 @@ func (tree *MerkleTree) Init(contents []Content) error {
 	if len(contents) == 0 {
 		return errors.New("content cannot be empty")
 	}
-	for _, content := range contents {
+	for i, content := range contents {
 		hash, err := content.CalcHash()
 		if err != nil {
 			return errors.New("could not calculate hash from content")
 		}
 		tree.Leaves = append(tree.Leaves, &Node{
-			Hash: hash,
-			Value: &content,
+			Hash:  hash,
+			Value: &contents[i],
 		})
 	}
-
 	root, err := buildTree(tree.Leaves)
 	if err != nil {
 		return errors.New("fail to build merkle tree out of ")
@@ -61,7 +86,7 @@ func buildTree(nodes []*Node) (Node, error) {
 		return *nodes[0], nil
 	}
 	var parents []*Node
-	for i:=0; i<len(nodes); i+=2 {
+	for i := 0; i < len(nodes); i += 2 {
 		hash := sha256.New()
 		// If odds number of nodes, construct the last parent with both left and right as nodes[-1].Hash
 		leftNode, rightNode := nodes[i], nodes[i]
@@ -72,11 +97,12 @@ func buildTree(nodes []*Node) (Node, error) {
 		if _, err := hash.Write(concatHash); err != nil {
 			return Node{}, errors.New("cannot hash content: " + string(concatHash))
 		}
-		parent := Node {
-			Hash: hash.Sum(nil),
-			Left: leftNode,
+		parent := Node{
+			Hash:  hash.Sum(nil),
+			Left:  leftNode,
 			Right: rightNode,
 		}
+		leftNode.Parent, rightNode.Parent = &parent, &parent
 		parents = append(parents, &parent)
 	}
 	return buildTree(parents)
@@ -84,10 +110,78 @@ func buildTree(nodes []*Node) (Node, error) {
 
 // GetProof returns a MerkleProof for given object. If the supplied object doesn't exist in the tree, return error.
 func GetProof(tree *MerkleTree, content Content) (pb.MerkleProof, error) {
-	return pb.MerkleProof{}, nil
+	for _, leaf := range tree.Leaves {
+		if leaf.Value == nil {
+			return pb.MerkleProof{}, errors.New("Leaf node cannot contain empty value, node hash: " + string(leaf.Hash))
+		}
+		// Found same content, construct and return the merkle proof.
+		if isEqual, _ := content.Equals(*leaf.Value); isEqual == true {
+			return computeMerkleProofFromLeaf(leaf, tree.Root)
+		}
+	}
+	return pb.MerkleProof{}, errors.New("does not find the content in tree: " + content.DebugString())
+}
+
+func computeMerkleProofFromLeaf(node *Node, root *Node) (pb.MerkleProof, error) {
+	if root.Parent != nil {
+		return pb.MerkleProof{}, errors.New("root is invalid, it contains parent")
+	}
+	proof := pb.MerkleProof{Root: root.Hash}
+	curNode := node
+	for curNode.Parent != nil {
+		sibling, err := getNodeSibling(curNode)
+		if err != nil {
+			return pb.MerkleProof{}, err
+		}
+		// Add proof pair into ProofPairs, in a bottom up way.
+		proof.ProofPairs = append(proof.ProofPairs, &pb.ProofPair{
+			Primary:   curNode.Hash,
+			Secondary: sibling.Hash,
+		})
+		curNode = curNode.Parent
+	}
+	// validate that now curNode should be root
+	if !isSameBytes(curNode.Hash, root.Hash) {
+		return pb.MerkleProof{}, errors.New("early abort before reaching parent, curNode hash is: " + string(curNode.Hash))
+	}
+	return proof, nil
+}
+
+// verifyHashToParent verifies that hash(left + right) == parent.
+func verifyHashToParent(left []byte, right []byte, parent []byte) bool {
+	hash := sha256.New()
+	if _, err := hash.Write(append(left, right...)); err != nil {
+		return false
+	}
+	return isSameBytes(hash.Sum(nil), parent)
 }
 
 // VerifyProof verifies a MerkleProof, it takes in a data list, and verify all the way to the end.
-func VerifyProof(proof pb.MerkleProof, data []byte) (bool, error) {
-	return false, nil
+func VerifyProof(proof pb.MerkleProof, content Content) bool {
+	// If proof just contain root, it should be a single node tree.
+	if proof.ProofPairs == nil {
+		contentHash, err := content.CalcHash()
+		return err == nil && isSameBytes(contentHash, proof.Root)
+	}
+
+	// Verify all the way to root hash.
+	for i, proofPair := range proof.ProofPairs {
+		if i+1 < len(proof.ProofPairs) {
+			parentPair := proof.ProofPairs[i+1]
+			if !verifyHashToParent(proofPair.Primary, proofPair.Secondary, parentPair.Primary) {
+				return false
+			}
+		} else {
+			if !verifyHashToParent(proofPair.Primary, proofPair.Secondary, proof.Root) {
+				return false
+			}
+		}
+	}
+
+	// Verify content hashes to first primary.
+	contentHash, err := content.CalcHash()
+	if err != nil || !isSameBytes(contentHash, proof.ProofPairs[0].Primary) {
+		return false
+	}
+	return true
 }
--- merkle/merkle_test.go
+++ merkle/merkle_test.go
@@ -1,12 +1,14 @@
 package merkle
+
 import (
 	sha256 "crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-// Defines a test content, which contains only 2 bytes array.
+// Defines a test content, which contains a string as value.
 type testContent struct {
 	x string
 }
@@ -20,15 +22,107 @@ func (tc *testContent) CalcHash() ([]byte, error) {
 }
 
 func (tc *testContent) Equals(content Content) (bool, error) {
-	return len(tc.x) == len(content.(*testContent).x), nil
+	return tc.x == content.(*testContent).x, nil
 }
 
-func TestBuildTree(t *testing.T) {
-	var contents []Content
-	contents = append(contents, &testContent{x: string([]byte{1, 1})})
-	contents = append(contents, &testContent{x: string([]byte{2, 2})})
+func (tc *testContent) DebugString() string {
+	return tc.x
+}
 
+func TestBuildTreeSingleNode(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"})
 	tree := MerkleTree{}
 	tree.Init(contents)
 	assert.NotNil(t, tree.Root.Hash)
+	hashHexString := hex.EncodeToString(tree.Root.Hash)
+	assert.Equal(t, "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", hashHexString)
+}
+
+func TestBuildTreeEvenNodes(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"}, &testContent{x: "b"})
+	tree := MerkleTree{}
+	tree.Init(contents)
+	assert.NotNil(t, tree.Root.Hash)
+	hashHexString := hex.EncodeToString(tree.Root.Hash)
+	assert.Equal(t, "e5a01fee14e0ed5c48714f22180f25ad8365b53f9779f79dc4a3d7e93963f94a", hashHexString)
+	assert.Equal(t, len(tree.Leaves), 2)
+	assert.Equal(t, (*tree.Leaves[0].Value).DebugString(), "a")
+}
+
+func TestBuildTreeOddNodes(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"}, &testContent{x: "b"}, &testContent{x: "c"})
+	tree := MerkleTree{}
+	tree.Init(contents)
+	assert.NotNil(t, tree.Root.Hash)
+	hashHexString := hex.EncodeToString(tree.Root.Hash)
+	assert.Equal(t, "d31a37ef6ac14a2db1470c4316beb5592e6afd4465022339adafda76a18ffabe", hashHexString)
+}
+
+func TestGetMerkleProof(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"}, &testContent{x: "b"}, &testContent{x: "c"})
+	tree := MerkleTree{}
+	tree.Init(contents)
+	merkleProof, err := GetProof(&tree, &testContent{x: "a"})
+	assert.Nil(t, err)
+	assert.Equal(t,
+		"d31a37ef6ac14a2db1470c4316beb5592e6afd4465022339adafda76a18ffabe",
+		hex.EncodeToString(merkleProof.Root))
+	assert.Equal(t,
+		"ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+		hex.EncodeToString(merkleProof.ProofPairs[0].Primary))
+}
+
+func TestGetMerkleProofSingleNode(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"})
+	tree := MerkleTree{}
+	tree.Init(contents)
+	merkleProof, err := GetProof(&tree, &testContent{x: "a"})
+	assert.Nil(t, err)
+	assert.Nil(t, merkleProof.ProofPairs)
+	assert.Equal(t,
+		"ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb",
+		hex.EncodeToString(merkleProof.Root))
+}
+
+func TestVerifyProofShouldReturnTrueForGetProof(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"}, &testContent{x: "b"}, &testContent{x: "c"})
+	tree := MerkleTree{}
+	tree.Init(contents)
+	merkleProof, _ := GetProof(&tree, &testContent{x: "a"})
+	assert.Equal(t, true, VerifyProof(merkleProof, &testContent{x: "a"}))
+
+	// Test negative.
+	assert.Equal(t, false, VerifyProof(merkleProof, &testContent{x: "b"}))
+}
+
+func TestVerifyProofSingleNode(t *testing.T) {
+	var contents []Content
+	contents = append(
+		contents,
+		&testContent{x: "a"})
+	tree := MerkleTree{}
+	tree.Init(contents)
+	merkleProof, _ := GetProof(&tree, &testContent{x: "a"})
+	assert.Equal(t, true, VerifyProof(merkleProof, &testContent{x: "a"}))
+
+	// Test negative.
+	assert.Equal(t, false, VerifyProof(merkleProof, &testContent{x: "b"}))
 }
