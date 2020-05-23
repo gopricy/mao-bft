f38920409edbd3980f5fde9fbe94e65c55641328
Boning <sdi.gbn@gmail.com>
2020-05-23 01:03:07 -0400

    fix erasure

--- rbc/erasure/erasure.go
+++ rbc/erasure/erasure.go
@@ -2,46 +2,55 @@ package erasure
 
 import (
 	"bytes"
+
 	"github.com/gopricy/mao-bft/pb"
 	"github.com/gopricy/mao-bft/rbc/merkle"
 	"github.com/klauspost/reedsolomon"
 	"github.com/pkg/errors"
 )
 
-func Split(data []byte, f, t int)([][]byte, error){
+func Split(data []byte, f, t int) ([][]byte, error) {
 	enc, err := reedsolomon.New(t-2*f, 2*f)
-	if err != nil{
+	if err != nil {
 		return nil, err
 	}
-	return enc.Split(data)
+	shards, err := enc.Split(data)
+	if err != nil {
+		return nil, err
+	}
+	err = enc.Encode(shards)
+	if err != nil {
+		return nil, err
+	}
+	return shards, nil
 }
 
-func Reconstruct(payloads []*pb.Payload, f, t int) ([]byte, error){
+func Reconstruct(payloads []*pb.Payload, f, t int) ([]byte, error) {
 	shards := make([][]byte, t)
-	for _, p := range payloads{
+	for _, p := range payloads {
 		i := merkle.GetLeafIndex(p.MerkleProof)
 		shards[i] = p.Data
 	}
 	return ReconstructBytes(shards, f)
 }
 
-func ReconstructBytes(shards [][]byte, f int) ([]byte, error){
-	enc, err := reedsolomon.New(len(shards) - 2 * f, 2 * f)
-	if err != nil{
+func ReconstructBytes(shards [][]byte, f int) ([]byte, error) {
+	enc, err := reedsolomon.New(len(shards)-2*f, 2*f)
+	if err != nil {
 		return nil, err
 	}
-	if err := enc.Reconstruct(shards); err != nil{
+	if err := enc.Reconstruct(shards); err != nil {
 		return nil, errors.Wrap(err, "Failed to reconstruct the data")
 	}
 	res := new(bytes.Buffer)
-	if err := enc.Join(res, shards, len(shards[0]) * (len(shards) - 2 * f)); err != nil{
+	if err := enc.Join(res, shards, len(shards[0])*(len(shards)-2*f)); err != nil {
 		return nil, errors.Wrap(err, "Failed to concat the data")
 	}
 	trim := res.Bytes()
 	for {
-		if trim[len(trim) - 1] == byte(0){
-			trim = trim[:len(trim) - 1]
-		} else{
+		if trim[len(trim)-1] == byte(0) {
+			trim = trim[:len(trim)-1]
+		} else {
 			break
 		}
 	}
--- rbc/erasure/erasure_test.go
+++ rbc/erasure/erasure_test.go
@@ -1,19 +1,20 @@
 package erasure_test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
+
+	"github.com/gopricy/mao-bft/rbc/erasure"
+	"github.com/stretchr/testify/assert"
 )
-import "github.com/gopricy/mao-bft/rbc/erasure"
 
 var testbytes []byte
 
-func init(){
+func init() {
 	testbytes = testData()
 }
 
-func testData() []byte{
+func testData() []byte {
 	res := make([]byte, rand.Intn(1000))
 	rand.Read(res)
 	return res
@@ -21,41 +22,40 @@ func testData() []byte{
 
 func TestSplit(t *testing.T) {
 	f := rand.Intn(5)
-	n := 3 * f + 1
+	n := 3*f + 1
 	shards, err := erasure.Split(testbytes, f, n)
 	assert.Nil(t, err)
 	assert.Equal(t, n, len(shards))
 	data := []byte{}
-	for i := 0; i < n - 2 * f; i ++ {
+	for i := 0; i < n-2*f; i++ {
 		data = append(data, shards[i]...)
 	}
 	for {
-		if data[len(data) - 1] == byte(0){
-			data = data[:len(data) - 1]
-		}else{
+		if data[len(data)-1] == byte(0) {
+			data = data[:len(data)-1]
+		} else {
 			break
 		}
 	}
 	assert.Equal(t, testbytes, data)
 }
 
-// The package we are using seems buggy, will try another one
-//func TestReconstruct(t *testing.T) {
-//	f := rand.Intn(5)
-//	n := 3 * f + 1
-//	shards, err := erasure.Split(testbytes, f, n)
-//	assert.Nil(t, err)
-//
-//	// set 2f shards to nil
-//	for i := 0; i < f; {
-//		r := rand.Intn(len(shards))
-//		if shards[r] != nil{
-//			shards[r] = nil
-//			i += 1
-//		}
-//	}
-//
-//	recov, err := erasure.ReconstructBytes(shards, f)
-//	assert.Nil(t, err)
-//	assert.Equal(t, testbytes, recov)
-//}
+func TestReconstruct(t *testing.T) {
+	f := rand.Intn(5)
+	n := 3*f + 1
+	shards, err := erasure.Split(testbytes, f, n)
+	assert.Nil(t, err)
+	assert.Equal(t, n, len(shards))
+	data := []byte{}
+	// set 2 * f shards to nil
+	for i := 0; i < 2*f; {
+		r := rand.Intn(len(shards))
+		if shards[r] != nil {
+			shards[r] = nil
+			i += 1
+		}
+	}
+	data, err = erasure.ReconstructBytes(shards, f)
+	assert.Nil(t, err)
+	assert.Equal(t, testbytes, data)
+}
